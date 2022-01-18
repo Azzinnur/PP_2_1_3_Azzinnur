1116a45780b69a3ad2052aaa3842b2b2aacb73d4
Azzinnur <hermin.chequetet@gmail.com>
2022-01-18 08:47:25 +0300

    pom update

--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -1,9 +1,11 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Cat extends Animal {
+    @Qualifier("cat")
     @Override
     public String toString() {
         return "Im a Cat";
